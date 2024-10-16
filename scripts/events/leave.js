bdb6d5855df77177f97db65a64a3367315514a52
Jonhs-rudeus10 <dieumerciciribagula@gmail.com>
2024-10-16 15:07:22 +0200

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -23,9 +23,9 @@ module.exports = {
 			session2: "noon",
 			session3: "afternoon",
 			session4: "evening",
-			leaveType1: "left",
-			leaveType2: "was kicked from",
-			defaultLeaveMessage: "{userName} {type} the group"
+			leaveType1: "a compris combien il était initile et a quitté le ",
+			leaveType2: "s'est fait chassé du",
+			defaultLeaveMessage: "{userName} {type} groupe"
 		}
 	},
 
