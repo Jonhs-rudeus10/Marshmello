42ff79b30fbd8f589960ed4fee76e35a20fadc5b
Ariel Ackerman <dieumerciciribagula@gmail.com>
2024-11-01 14:48:35 +0200

    Update vortexgc.js

--- botgc.js
+++ botgc.js
@@ -3,7 +3,7 @@ const moment = require('moment-timezone');
 
 module.exports = {
   config: {
-    name: "botgc",
+    name: "vortexgc",
     aliases: ["vortexgc, arielgc"],
     version: "1.0",
     author: "ariel ackerman",
