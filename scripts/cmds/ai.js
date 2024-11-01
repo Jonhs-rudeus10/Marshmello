0db95b63852512cfa1580698b54e367a97b1dc49
Ariel Ackerman <dieumerciciribagula@gmail.com>
2024-11-01 14:52:32 +0200

    Update ai.js

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -15,7 +15,7 @@ async function getAIResponse(input, userName, userId, messageID) {
  { url: 'https://ai-chat-gpt-4-lite.onrender.com/api/hercai', params: { question: input } }
  ];
 
- let response = ` Salut petit humain moi c'est 𝐚𝐫𝐢𝐞𝐥 𝐯𝐨𝐫𝐭𝐞𝐱 𝐛𝐥𝐚𝐝𝐞 une intelligence artificielle créé par Ariel Ackerman 😏`;
+ let response = ` Salut petit humain moi c'est 𝐯𝐨𝐫𝐭𝐞𝐱 𝐛𝐥𝐚𝐝𝐞 une intelligence artificielle créé par Ariel Ackerman 😏`;
  let currentIndex = 0;
 
  for (let i = 0; i < services.length; i++) {
@@ -43,7 +43,7 @@ module.exports = {
  onStart: async function ({ api, event, args }) {
  const input = args.join(' ').trim();
  if (!input) {
- api.sendMessage("Salut petit humain je suis 𝐚𝐫𝐢𝐞𝐥 𝐯𝐨𝐫𝐭𝐞𝐱 𝐛𝐥𝐚𝐝𝐞 une intelligence artificielle créé par ariel Ackerman 😏", event.threadID, event.messageID);
+ api.sendMessage("Salut petit humain je suis 𝐯𝐨𝐫𝐭𝐞𝐱 𝐛𝐥𝐚𝐝𝐞 une intelligence artificielle créé par ariel Ackerman 😏", event.threadID, event.messageID);
  return;
  }
 
@@ -54,7 +54,7 @@ module.exports = {
  }
  const userName = ret[event.senderID].name;
  const { response, messageID } = await getAIResponse(input, userName, event.senderID, event.messageID);
- api.sendMessage(`★𝙖𝙧𝙞𝙚𝙡 𝙖𝙘𝙠𝙚𝙧𝙢𝙖𝙣★ \n✧══════•❁❀❁•══════✧\n${response}\n✧══════•❁❀❁•══════✧ `, event.threadID, messageID);
+ api.sendMessage(`★𝙖𝙧𝙞𝙚𝙡 𝙖𝙘𝙠𝙚𝙧𝙢𝙖𝙣★ \n✧══════•❁❀❁•══════✧\n${response}\n          🥴🚬           ✧══════•❁❀❁•══════✧ `, event.threadID, messageID);
  });
  },
  onChat: async function ({ api, event, message }) {
